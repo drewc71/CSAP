f9361a9cd4002e051c30dec0062deb6ceaff71b0
Drew Carlson <drewc@outlook.com>
2022-04-07 23:51:35 -0700

    Create khet.java

---
+++ khet.java
@@ -0,0 +1,8 @@
+public enum Piece { Pyramid, Pharoah, Obelisk }
+public enum Dir { N, NE, E, SE, S, SW, W, NW }
+
+class Khet
+{
+  public void SetPiece(int row, int col, Piece piece, Dir dir){}
+  public void FireLazer(int row, int col, Dir dir) {}
+}
